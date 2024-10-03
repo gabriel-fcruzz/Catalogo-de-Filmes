5e90f8e192c89f13414cd733e466b1221d84bbb3
Gabriel Cruz Ferreira <143708127+gabriel-fcruzz@users.noreply.github.com>
2024-10-03 03:38:53 +0000

    teste vercel1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import "../styles/Header.module.css";
 
 const Header = () => {
   return (
-    <header>
+    <header className={styles.header}>
       <h1>Catálogo de Filmes</h1>
       <Navbar />
     </header>
